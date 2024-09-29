3b4ee4116c06b28b1bf517849f39084e957ddeac
Samrawit Tekheste <samrawit.samuel.tk@gmail.com>
2024-09-29 22:09:47 +0000

    feat:Add form to the register html and use crispy Django

--- flav/settings.py
+++ flav/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'fusion',
     'users',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- users/views.py
+++ users/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from . import forms
 
-# Create your views here.
+
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+  if request.method == "POST":
+    form = forms.UserRegisterForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        messages.success(request, f"{username}, you're account has been created!")
+        return redirect('home-flav')
+  else:
+
+   form = forms.UserRegisterForm()
+  return render(request, 'register.html', {'form': form})
 
 
