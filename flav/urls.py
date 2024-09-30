9fbe9c1952ba1b9eef52212194086d60484ae846
Samrawit Tekheste <samrawit.samuel.tk@gmail.com>
2024-09-30 22:27:21 +0000

    fix:Heroku to connect properly

--- flav/urls.py
+++ flav/urls.py
@@ -18,10 +18,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include # import 'include' to connect app URLS
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('', include('fusion.urls')), # includes URLS from the app
     path('register/', user_views.register, name="user-register"),
+    path('login/', auth_views.LoginView.as_view(template_name="user/login.html"), name="user-login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name="user/logout.html"), name="user-logout"),
+    
 ]
